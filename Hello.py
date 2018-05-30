8470592a7eadfddc6bf16c7a6da59fee29381e28
Sindhura Palakodety <sindhupalakodety@gmail.com>
2018-05-30 15:51:26 -0700

    test remote

--- Hello.py
+++ Hello.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import os
-     
+import socket
+
+ssh $UserName@$ServerName "hostname -f"
+
 path = '.'
 files = os.listdir(path)
 
 for name in files:
    print(name)
+
+
